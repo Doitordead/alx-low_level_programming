31b703ce1164c458afa64d2d28544b77114573a3
dictiionnary <dictionnary15@gmail.com>
2022-09-09 08:24:11 -0700

    1-last_digit.c sixth commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -34,7 +34,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %i is %i and is less than 5\n",n, lastDigit);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n",n, lastDigit);
 	}
 
 	return (0);
