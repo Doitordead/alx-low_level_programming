c9b4ff401f6d5fe4746c747806a4e5d956e68abe
dictiionnary <dictionnary15@gmail.com>
2022-09-09 09:43:12 -0700

    5-print_numbers.c first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry
+ *
+ * Description:
+ *
+ * Return: return 0 (Succes)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (numbers <= 9)
+	{
+		printf("%i", number);
+			++number;
+	}
+	printf("\n");
+
+	return (0);
+}
