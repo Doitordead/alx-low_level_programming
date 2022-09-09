dd4c78837d77aeab961f1f5f2c4031e2c9a8c112
dictiionnary <dictionnary15@gmail.com>
2022-09-09 09:49:57 -0700

    6-print_numberz.c first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry
+ *
+ * Description:
+ *
+ * Return: return 0 (Succes)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number);
+		++number;
+	}
+	putchar('\n');
+
+	return (0);
+}
