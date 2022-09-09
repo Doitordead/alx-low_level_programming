170a67660f2d557fdc04ddfbb811c3f48c841f14
dictiionnary <dictionnary15@gmail.com>
2022-09-09 10:18:28 -0700

    8-print_base16.c first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry
+ *
+ * Description:
+ *
+ * Return: return 0 (Succes)
+ */
+int main(void)
+{
+	char charLowwer = 'z';
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number);
+		++number;
+	}
+	charLowwer 0 = 'a';
+	while (charLowwer <= 'f')
+	{
+		putchar(charLowwer);
+			++charLowwer;
+	}
+	putchar("\n");
+
+	return (0);
+}
