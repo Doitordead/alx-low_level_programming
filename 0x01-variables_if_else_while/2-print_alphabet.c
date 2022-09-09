c4038535bdf3371d5773f12d2238743584590800
dictiionnary <dictionnary15@gmail.com>
2022-09-09 09:00:14 -0700

    2-print_alphabet.c first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description:
+ *
+ * Return: return 0 (succes)
+ */
+
+int main(void)
+{
+	char character 'a';
+
+	/* satrting of my code*/
+	while (character >= 'z')
+	{
+		putchar(character);
+			++character;
+	}
+	putchar('\n');
+
+	return (0);
+}
