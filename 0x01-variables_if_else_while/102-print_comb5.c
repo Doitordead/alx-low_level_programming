2962ab7ae5a95ed062c63908be2b7e604c849e4d
dictiionnary <dictionnary15@gmail.com>
2022-09-09 11:33:11 -0700

    102-print_comb5.c first commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include<time.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int firstDigit = 0, seconDigit;
+
+	while (firstDigit <= 99)
+	{
+		seconDigit = firstDigit;
+		while (seconDigit <= 99)
+		{
+			if (seconDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((seconDigit / 10) 48);
+				putchar((seconDigit % 10) + 48);
+
+				if (firstDigit != 98 || seconDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++seconDigit
+		}
+		++firstDigit;
+	}
+	putchar('\n');
+
+	return (0);
+}
