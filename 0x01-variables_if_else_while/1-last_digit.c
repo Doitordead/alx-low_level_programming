299fd3f3f4f1d8ad57e7fedf3849a53e14e602d7
dictiionnary <dictionnary15@gmail.com>
2022-09-09 08:42:53 -0700

    1-last_digit.c tenth commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,14 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - Entry function
  *
  * Description: This program will assign a random number to the
  *		variable n each time it is executed. Complete
  *		the source code in order to print the last
  *		digit of the number stored in the variable n.
- *description:
+ *
  * Return: the function return 0 if succed
  */
 
