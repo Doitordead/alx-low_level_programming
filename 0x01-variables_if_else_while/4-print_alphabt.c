0f668d95a896d790d483e762acfe7f4f0c478326
dictiionnary <dictionnary15@gmail.com>
2022-09-09 09:32:36 -0700

    4-print_alphabt.c first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry
+ *
+ * Description:
+ *
+ * Return: return 0 (Succes)
+ */
+int main(void)
+{
+	char charLowwer = 'a';
+
+	while (charLowwer <= 'z')
+	{
+		if (charLowwer == e || chartLowwer == q)
+		{
+			++charLowwer;
+		}
+		else ()
+		{
+			putchar(charLowwer);
+			++charLowwer;
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
