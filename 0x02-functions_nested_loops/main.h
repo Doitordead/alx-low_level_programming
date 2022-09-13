e0e2e677e2a1bb1ff36a0bfe68168836e35fd005
dictiionnary <dictionnary15@gmail.com>
2022-09-13 16:22:54 -0700

    all 2sd

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,12 @@
 #define ALX_INCLUDED
 
 int _putchar(char);
-
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _isLower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void)
 #endif
