67397fcc2ecfabec9f64f20bbe3a9f50e60cac7c
dictiionnary <dictionnary15@gmail.com>
2022-09-13 14:43:37 -0700

    0-putchar.c first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main -Entry point
+ *
+ * Description:
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+	putchar("\n");
+
+	return (0);
+}
+
