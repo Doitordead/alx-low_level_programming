ce6eaf4d3321e1ebda4203df5edda300c809d2f1
dictiionnary <dictionnary15@gmail.com>
2022-09-13 18:45:05 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,20 @@
-#include "main.h"
+#include"main.h"
 /**
- * add - sums up two numbers
- *
- * @n:first integer
- *
- * @m:second integer
- *
- * Return: returns 0
+ * print_to_98 - print n to 98 counts
+ * separated by comma, followed
+ * by space and number should be
+ * printed in order
+ * @n: input
  */
-int add(int n, int m)
+void print_to_98(int n)
 {
-	return (n + m);
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
 }
