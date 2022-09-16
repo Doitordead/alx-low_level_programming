4d239df38382ade5a53f24b35e0da962dd1162bf
dictiionnary <dictionnary15@gmail.com>
2022-09-15 17:42:31 -0700

    2-mul.c 1st commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ *mul - function that multiflies two numbers
+ *@a: first number
+ *@b: second number
+ *Return: return resul
+ */
+
+int mul(int a, int b)
+{
+	int a, b, c;
+
+	c = a * b;
+
+	return (c);
+}
