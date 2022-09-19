a2a9f9084eea4e47defa1c43fb5188788e508387
dictiionnary <dictionnary15@gmail.com>
2022-09-19 08:26:49 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: the string parameter
+ *
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
