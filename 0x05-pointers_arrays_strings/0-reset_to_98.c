5ef13d53866c43fc9056b65c7aec635b65052034
dictiionnary <dictionnary15@gmail.com>
2022-09-19 08:05:45 -0700

    all commmit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - take a pointrer to an int as parameter
+ *		and update the value it point to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
