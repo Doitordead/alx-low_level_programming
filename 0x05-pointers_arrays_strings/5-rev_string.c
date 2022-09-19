38870b55a43486bbcfcdfe3f360a6cfbd9550884
dictiionnary <dictionnary15@gmail.com>
2022-09-19 09:33:00 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: the parameter
+ *
+ * Return nothing
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char str;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+
+	}
+	for (j = 0; < i / 2; ++j)
+	{
+		str = s[i];
+		s[i] = s[i - 1 - i];
+		s[i - 1 - i] = str;
+	}
+}
