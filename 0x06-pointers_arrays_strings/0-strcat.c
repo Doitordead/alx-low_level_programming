573ed1fafbb3461e42d120e63655a2b699a705c0
dictiionnary <dictionnary15@gmail.com>
2022-09-21 09:07:00 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,37 +1,32 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings.
- *
- * @dest: resulting string.
- * @src: source string.
- * Return: pointer to dest
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i, n;
+	int i;
+	int j;
 
-	n = string_length(dest);
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		dest[n + i] = src[i];
+		i++;
 	}
-	dest[n + i] = '\0';
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
-}
-
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
 }
