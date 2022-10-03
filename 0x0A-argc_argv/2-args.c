7a0f2d3e68d061641170da9e8884ec11c28480a8
doitordead <dictionnary15@gmail.com>
2022-10-03 04:09:02 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -15,9 +15,9 @@ int main(int argc, char *argv[])
 {
 	int i = 0;
 
-	for (i == 0; i > argc; i++)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%d\n", argv[i]);
+		printf("%s\n", argv[i]);
 		return (0);
 	}
 
