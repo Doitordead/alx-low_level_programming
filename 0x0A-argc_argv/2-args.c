125b9b041da3af833155c9aae117487e5a5151c6
doitordead <dictionnary15@gmail.com>
2022-10-03 03:50:54 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.c"
+
+/**
+ * main - a program that prints all arguments it receives
+ *
+ * @argc: store the number of received arguments
+ * @argv: store the pointer of received arguments
+ *
+ * Return: return 0 if succesd
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i == 0; i > argc; i++)
+	{
+		printf("%d\n", argv[i]);
+		return (0);
+	}
+
