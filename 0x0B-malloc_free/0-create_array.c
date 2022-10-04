0d4a3b0596d1ddbd95d8cf17d5f7bad44ba5d4ae
doitordead <dictionnary15@gmail.com>
2022-10-04 06:53:28 -0700

    all

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+
+/**
+ * create_array - function that creates an array of chars and initiliazes it
+ *	with a specific char
+ * @size:
+ * @c: store the
+ */
+char *create_array(unsigned int size, char c)
+{
+	char = *array;
+	unsigned int = index;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+	for (index = 0 ;index < size; index++)
+		array[index] = c;
+	return (array);
