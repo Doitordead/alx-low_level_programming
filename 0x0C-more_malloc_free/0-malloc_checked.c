cfd100524ed0c6c182665a8410450a506fd5c4dd
doitordead <dictionnary15@gmail.com>
2022-10-07 17:50:16 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 /**
- * alloc_checked - allocates memory using malloc
+ * malloc_checked - allocates memory using malloc
  * @b: amount of bytes
  * Return: pointer to our new allocated memory
  * exit with 98 if malloc fails
@@ -10,12 +10,12 @@
 void *malloc_checked(unsigned int b)
 {
 	void *new_mem;
-	
+
 	new_mem = malloc(b);
 
 	if (new_mem == NULL)
 		exit(98);
-	
+
 	return (new_mem);
 
 }
